6d91b73c46ff948b692da57c9352bbdf3a222624
SamyJanatiUpssitech <samy.janati@univ-tlse3.fr>
2025-02-20 08:53:46 +0100

    retp3

--- src/jeu/Affichage.java
+++ src/jeu/Affichage.java
@@ -1,5 +1,24 @@
 package jeu;
 
+import java.util.Scanner;
+import joueur.Joueur;
+import objetsJeu.Carte;
+
 public class Affichage {
+	
+	private Scanner scanner=new Scanner(System.in);
+	
+	public void afficherNbTour(int nbTour) {
+		System.out.println("nb tour : "+nbTour+"\n\n");
+	}
+	
+	public void nouveuJoueur(int numJoueur) {
+		System.out.println("Nom du nouveau joueur : ");
+		String nom = scanner.next();
+		System.out.println("\n" + nom+" a été crée\n");
+	}
+	public static void main(String[]args) {
+		
+	}
 
 }
