f1aa3af2524eaba21f53700b307e57ba55baf94e
SamyJanatiUpssitech <samy.janati@univ-tlse3.fr>
2025-02-20 09:31:00 +0100

    fintp3

--- src/jeu/Affichage.java
+++ src/jeu/Affichage.java
@@ -12,12 +12,22 @@ public class Affichage {
 		System.out.println("nb tour : "+nbTour+"\n\n");
 	}
 	
-	public void nouveuJoueur(int numJoueur) {
+	public void nouveauJoueur(int numJoueur) {
 		System.out.println("Nom du nouveau joueur : ");
 		String nom = scanner.next();
 		System.out.println("\n" + nom+" a été crée\n");
 	}
+	
+	public void initPioche() {
+        System.out.println("Création pioche (40 cartes)\n\n");
+    }
+	
+	
+	
 	public static void main(String[]args) {
+		
+		Affichage affichage = new Affichage();
+		affichage.nouveauJoueur(1);
 		
 	}
 
